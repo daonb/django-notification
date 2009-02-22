60bea349a96a0a57ea854295ffe157d3a7bc849d
Benny Daon <bennydaon@gmail.com>
2009-02-22 14:57:47 +0200

    improved emit_digest to use templates and added template files.

--- notification/management/commands/emit_digest.py
+++ notification/management/commands/emit_digest.py
@@ -1,15 +1,17 @@
 from django.conf import settings
 from django.core.management.base import NoArgsCommand
+from django.template import Context
+from django.template.loader import render_to_string
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.sites.models import Site
+
 # favour django-mailer but fall back to django.core.mail
 from django.core.mail import EmailMessage, SMTPConnection
 from notification.model import Notice, DIGEST_MEDIUM, should_send
 
-EMAIL_SUBJECT = getattr(settings, 'NOTIFICATION_DIGEST_SUBJECT', _('%(site_name)s - %(count)s new notices - digested'))
 class Command(NoArgsCommand):
-    help = "Emit digest emails."
+    help = "send digest emails for all users."
     
     def handle_noargs(self, **options):
         current_site = Site.objects.get_current()
@@ -17,13 +19,15 @@ class Command(NoArgsCommand):
         smtp = SMTPConnection()
         smtp.open()
         for user in User.objects.filter(email__isnull=False):
-            n = []
+            notices = []
             for notice in Notice.objects.notices_for(user):
                 if should_send(user, notice.notice_type, DIGEST_MEDIUM):
-                    n.append(notice.message)
+                    notices.append(notice.message)
                     notice.archive()
-            body = '<br />'.join(n)
-            subject = EMAIL_SUBJECT % dict(site_name=current_site.name, count=len(n))
+            context = Context({'notices': notices, 'user': user})
+            body =  render_to_string ('notification/digest_email_body.html', context_instance=context)
+            context.autoescape = False
+            subject = render_to_string ('notification/digest_email_message.txt', context_instance=context) 
             msg = EmailMessage(subject, body, settings.DEFAULT_FROM_EMAIL, [user.email])
             msg.content_subtype = "html"  # Main content is now text/html
             smtp._send(msg)
