14dd7c4abe9d676b703970219f6d23f485882d93
Benny Daon <bennydaon@gmail.com>
2009-02-26 17:53:19 +0200

    added the missing templates and fixed a couple of bugs. still not sure if it sends anything.

--- notification/management/commands/emit_digest.py
+++ notification/management/commands/emit_digest.py
@@ -8,7 +8,7 @@ from django.contrib.sites.models import Site
 
 # favour django-mailer but fall back to django.core.mail
 from django.core.mail import EmailMessage, SMTPConnection
-from notification.model import Notice, DIGEST_MEDIUM, should_send
+from notification.models import Notice, DIGEST_MEDIUM, should_send
 
 class Command(NoArgsCommand):
     help = "send digest emails for all users."
@@ -24,11 +24,13 @@ class Command(NoArgsCommand):
                 if should_send(user, notice.notice_type, DIGEST_MEDIUM):
                     notices.append(notice.message)
                     notice.archive()
-            context = Context({'notices': notices, 'user': user})
-            body =  render_to_string ('notification/digest_email_body.html', context_instance=context)
-            context.autoescape = False
-            subject = render_to_string ('notification/digest_email_message.txt', context_instance=context) 
-            msg = EmailMessage(subject, body, settings.DEFAULT_FROM_EMAIL, [user.email])
-            msg.content_subtype = "html"  # Main content is now text/html
-            smtp._send(msg)
+            if notices:
+                import pdb
+                # pdb.set_trace()
+                context = Context({'notices': notices, 'user': user})
+                body =  render_to_string ('notification/digest_email_body.html', context_instance=context)
+                subject = render_to_string ('notification/digest_email_subject.txt', {'count': len(notices)}) 
+                msg = EmailMessage(subject, body, settings.DEFAULT_FROM_EMAIL, [user.email])
+                msg.content_subtype = "html"  # Main content is now text/html
+                smtp._send(msg)
         smtp.close()    
